ca6ff92851e7180a4cbcddec04222bc7c3d09d23
Vladimir Sedikh <andiwillfly@dev.casino-x.com>
2017-10-03 15:34:19 +0300

    Test

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const Express = require('express');
 const React = require('react');
-const learnedRoute = require('./routes/learned.get.route');
-const currentTransfers = require('./routes/currentTransfers.get.route');
+const learnedRoute = require('./server/routes/learned.get.route');
+const currentTransfers = require('./server/routes/currentTransfers.get.route');
 
 
 const MongoClient = require('mongodb').MongoClient;
@@ -14,21 +14,12 @@ MongoClient.connect("mongodb://andiwillfly:ward121314@ds127854.mlab.com:27854/ne
 	const app = Express();
 	// Serve built files with express static files middleware
 	console.log('==========3', path.join(__dirname, 'built'));
-	app.use('/built', Express.static(path.join(__dirname, '../built')));
+	app.use('/built', Express.static(path.join(__dirname, './built')));
 
 	// Serve normal requests with our handleRender function
-	app.use('/static', Express.static(path.join(__dirname, '../static')));
-
-
-	//app.use('/players', (req, res)=> playersRoute(DB, req, res) );
+	app.use('/static', Express.static(path.join(__dirname, './static')));
 
 	app.use('/learned', (req, res)=> learnedRoute(DB, req, res) );
-
-	//app.post('/learn_save_player', (req, res)=> learnSavePlayerRoute(DB, req, res) );
-
-	//app.use('/keepers', (req, res)=> keepersRoute(DB, req, res) );
-
-	//app.use('/parse', (req, res)=> parseRoute(DB, req, res) );
 
 	app.use('/current_transfers', (req, res)=> currentTransfers(DB, req, res) );
 
@@ -42,6 +33,11 @@ MongoClient.connect("mongodb://andiwillfly:ward121314@ds127854.mlab.com:27854/ne
 	});
 
 
-	app.listen(3000);
+	app.listen(process.env.PORT || 3000);
+
+
+	// app.listen(app.get('port'), function() {
+	// 	console.log('Node app is running on port', app.get('port'));
+	// });
 	console.log('=== Go to http://localhost:3000 ===');
 });
